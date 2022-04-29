54439d66e3a1506d2b77f185e194d9af2d5b1d6e
HarshithaMWikiance <103238592+HarshithaMWikiance@users.noreply.github.com>
2022-04-29 13:49:46 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,33 @@
+import  React from 'react';
+import './App.css';
+
+
+function App() {
+  return(
+    <div className='App'>
+      <div class="card">
+    
+  <div class="card-image"></div>
+  <div class="card-text">
+    <h3>Id:1</h3>
+    <h3>Name:Leanne Graham</h3>
+    <p>UserName:Bret</p>
+    <p>Email:Sincere@april.biz</p>
+    <p>Address:
+      street: Kulas Light
+      city: Gwenborough,
+      zipcode: 92998-3874</p>
+    <p>Phone:1-770-736-8031 x56442</p> 
+    <p>website: hildegard.org</p>
+  </div>
+  </div>
+ 
+
+</div>
+
+  
+
+    
+    );
+  }
+  export default App;
